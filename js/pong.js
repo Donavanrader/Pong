112b50e648a783e8f8fe43da6c29892516adae6b
Donavanrader <shaggyrader@gmail.com>
2017-09-08 02:03:54 +0100

    Added some collision detection

--- js/pong.js
+++ js/pong.js
@@ -35,7 +35,7 @@ window.onload = function() {
 var step = function() {
 	update();
 	render();
-	animate();
+	animate(step);
 };
 
 var update = function() {
@@ -83,7 +83,7 @@ Computer.prototype.render = function() {
 function Ball(x, y) {
 	this.x = x;
 	this.y = y;
-	this.x_speed = 0;
+	this.x_speed = -10;
 	this.y_speed = 0;
 	this.radius = ballRadius;
 }
@@ -104,4 +104,47 @@ var render = function() {
 	player.render();
 	computer.render();
 	ball.render();
+};
+
+var update = function() {
+	ball.update(player.paddle, computer.paddle);
+};
+
+Ball.prototype.update = function(paddle1, paddle2) {
+	this.x += this.x_speed;
+	this.y += this.y_speed;
+	var top_x = this.x - ballRadius;
+	var top_y = this.y - ballRadius;
+	var bottom_x = this.x + ballRadius;
+	var bottom_y = this.y +ballRadius;
+	
+	// If the ball hits the left wall
+	//( the wall behind the player )
+	// a point is scored, and the ball is reset.
+	if (top_x < 0 || top_x > canvas.width) {
+		this.x_speed = -10;
+		this.y_speed = 0;
+		this.x = (canvas.width/2) - ballRadius;
+		this.y = (canvas.height/2) - ballRadius;
+	}
+	
+	// If the ball hits one of the vertical walls,
+	//it gets bounced back into the Pong Arena
+	if (top_y < 0) {
+		// Ball has hit the top wall
+		this.y = ballRadius;
+		this.y_speed = -this.y_speed;
+	} else if ( bottom_y > canvas.height ) {
+		// Ball has hit the bottom wall
+		this.y = canvas.height - ballRadius;
+		this.y_speed = -this.y_speed;
+	}
+	
+	// If paddle is hit. . .
+	var playerHitX = paddle1.x + paddle1.width;
+	if ( (playerHitX > top_x && playerHitX < bottom_x) && (paddle1.y < top_y && paddle1.y + paddle1.height > bottom_y) ) {
+		this.x_speed = 10;
+		this.y_speed += (paddle1.x_speed / 2);
+		this.x += this.x_speed;
+	}
 };
